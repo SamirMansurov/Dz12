96f662b562ff23816f9f7a0f52de96c020988019
Samir <samirmansuriv078@gmail.com>
2024-01-20 17:49:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// №1
+let change = document.querySelector('#name')
+change.innerText = '1234567890'
+
+
+// №2
+//  let arr =['Alex', 'John', 'Constantine', 'Tyler', 'Kacbachok', 'Smith']
+
+// let people = document.querySelectorAll('.people')
+
+// for (let elem of people) {
+//     elem.innerText = Math.random(arr)
+   
+// }console.log(people);
+
+
